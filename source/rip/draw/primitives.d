3e0dca88a04dcac91b41feb524d92f360b04dbd1
aquaratixc <disconnectix@gmail.com>
2016-06-01 21:57:05 +0400

    Update primitives.d

--- source/rip/draw/primitives.d
+++ source/rip/draw/primitives.d
@@ -45,7 +45,7 @@ auto drawDDALine(T, U, V, W)(Surface surface, RGBColor rgbColor,T x1, U y1, V x2
 }
 
 // Рисование линии методом Брезенхема
-void drawBresenhamLine(T, U, V, W)(Surface surface, RGBColor color, T x1, U y1, V x2, W y2)
+auto drawBresenhamLine(T, U, V, W)(Surface surface, RGBColor color, T x1, U y1, V x2, W y2)
 	if (allArithmetic!(T, U, V, W))
 {
 	float a = cast(float) x1;
@@ -94,7 +94,7 @@ void drawBresenhamLine(T, U, V, W)(Surface surface, RGBColor color, T x1, U y1, 
 }
 
 // Рисование окружности
-void drawCircle(T, U, V)(Surface surface, RGBColor color, T x, U y, V r)
+auto drawCircle(T, U, V)(Surface surface, RGBColor color, T x, U y, V r)
 	if (allArithmetic!(T, U, V))
 {	
 	assert (r >= 0);
@@ -112,7 +112,7 @@ void drawCircle(T, U, V)(Surface surface, RGBColor color, T x, U y, V r)
 }
 
 // Рисование конических сечений
-void drawConicSection(T, U, V, W)(Surface surface, RGBColor color, T x, U y, V l, W e)
+auto drawConicSection(T, U, V, W)(Surface surface, RGBColor color, T x, U y, V l, W e)
 	if (allArithmetic!(T, U, V, W))
 {
 	auto a = cast(float) x;
@@ -129,9 +129,28 @@ void drawConicSection(T, U, V, W)(Surface surface, RGBColor color, T x, U y, V l
 	}
 }
 
+// Рисование треугольника
+auto drawTriangle(P, Q, R, S, T, U)(Surface surface, RGBColor color, P x1, Q y1, R x2, S y2, T x3, U y3)
+	if (allArithmetic!(P, Q, R, S, T, U))
+{
+	auto a = cast(int) x1;
+	auto b = cast(int) y1;
+	auto c = cast(int) x2;
+	auto d = cast(int) y2;
+	auto e = cast(int) x3;
+	auto f = cast(int) y3;
+
+	surface.drawPoint(color, a, b);
+	surface.drawPoint(color, c, d);
+	surface.drawPoint(color, e, f);
+
+	surface.drawDDALine(color, a, b, c, d);
+	surface.drawDDALine(color, c, d, e, f);
+	surface.drawDDALine(color, e, f, a, b);
+}
 
 // Рисование прямоугольника
-void drawRectangle(T, U, V, W)(Surface surface, RGBColor color, T x, U y, V w, W h)
+auto drawRectangle(T, U, V, W)(Surface surface, RGBColor color, T x, U y, V w, W h)
 	if (allArithmetic!(T, U, V, W))
 {
 	assert(w >= 0);
@@ -164,7 +183,7 @@ void drawRectangle(T, U, V, W)(Surface surface, RGBColor color, T x, U y, V w, W
 }
 
 // Окружность с заливкой
-void drawFilledCircle(T, U, V)(Surface surface, RGBColor color, T x, U y, V r)
+auto drawFilledCircle(T, U, V)(Surface surface, RGBColor color, T x, U y, V r)
 	if (allArithmetic!(T, U, V))
 {
 	auto a = cast(float) x;
@@ -182,8 +201,90 @@ void drawFilledCircle(T, U, V)(Surface surface, RGBColor color, T x, U y, V r)
 	}
 }
 
+// Треугольник с заливкой
+auto drawFilledTriangle(P, Q, R, S, T, U)(Surface surface, RGBColor color, P x1, Q y1, R x2, S y2, T x3, U y3)
+	if (allArithmetic!(P, Q, R, S, T, U))
+{
+	// рисуем треугольник обычный
+	auto a = cast(float) x1;
+	auto b = cast(float) y1;
+	auto c = cast(float) x2;
+	auto d = cast(float) y2;
+	auto e = cast(float) x3;
+	auto f = cast(float) y3;
+	
+	surface.drawPoint(color, a, b);
+	surface.drawPoint(color, c, d);
+	surface.drawPoint(color, e, f);
+	
+	surface.drawDDALine(color, a, b, c, d);
+	surface.drawDDALine(color, c, d, e, f);
+	surface.drawDDALine(color, e, f, a, b);
+
+	// вычисление знаменателя для барицентрических координат
+	auto calculateDenominator(float X1, float Y1, float X2, float Y2, float X3, float Y3)
+	{
+		return ((Y2 - Y3) * (X1 - X3)) + ((X3 - X2) * (Y1 - Y3));
+	}
+
+	// вычисляем первую барицентрическую координату
+	auto calculateL1(float x, float y)
+	{
+		auto numerator = ((b - f) * (x - e)) + ((e - c) * (y - f));
+		auto denominator = calculateDenominator(a, b, c, d, e, f);
+		return numerator / denominator;
+	}
+
+	// вычисляем вторую барицентрическую координату
+	auto calculateL2(float x, float y)
+	{
+		auto numerator = ((f - a) * (x - e)) + ((a - e) * (y - f));
+		auto denominator = calculateDenominator(a, b, c, d, e, f);
+		return numerator / denominator;
+	}
+
+	// вычисляем третью барицентрическую координату
+	auto calculateL3(float L1, float L2)
+	{
+		return 1.0 - L1 - L2;
+	}
+
+	// подготовка данных для цикла отрисовки
+	auto xmin = min(a, c, e);
+	auto xmax = max(a, c, e);
+	auto ymin = min(b, d, f);
+	auto ymax = max(b, d, f);
+
+	// Находиться ли величина в интервале [0..1]
+	bool isBaricentric(float value)
+	{
+		return ((value >= 0) && (value <= 1.0));
+	}
+
+	// Находиться ли точка внутри плоскости, ограниченной линиями треугольника
+	bool isInsideOfTriangle(float x, float y)
+	{
+		auto L1 = calculateL1(x, y);
+		auto L2 = calculateL2(x, y);
+		auto L3 = calculateL1(L1, L2);
+		return ((isBaricentric(L1) && isBaricentric(L2) && isBaricentric(L3)));
+	}
+
+	// заливка треугольника
+	for (float x = xmin; x < xmax; x += 0.1)
+	{
+		for (float y = ymin; y < ymax; y += 0.1)
+		{
+			if (isInsideOfTriangle(x, y))
+			{
+				surface[cast(int) x, cast(int) y] = color;
+			}
+		}
+	}
+}
+
 // Прямоугольник с заливкой
-void drawFilledRectangle(T, U, V, W)(Surface surface, RGBColor color, T x, U y, W w, H h)
+auto drawFilledRectangle(T, U, V, W)(Surface surface, RGBColor color, T x, U y, W w, H h)
 	if (allArithmetic!(T, U, V, W))
 {
 	assert(w >= 0);
