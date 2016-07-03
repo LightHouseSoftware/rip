f4e0349350907206a707eb44674a5245ccb98796
VlasovRoman <vlasovroman.ru@yandex.ru>
2016-07-03 16:05:18 +0700

    Added missed modules in rip.processing

--- source/rip/processing/package.d
+++ source/rip/processing/package.d
@@ -1,6 +1,10 @@
 module rip.processing;
 
 public {
+    import rip.processing.bitPlane;
+    import rip.processing.colorization;
+    import rip.processing.order;
+    import rip.processing.rough;
     import rip.processing.filters;
     import rip.processing.grayscale;
     import rip.processing.convolution;
