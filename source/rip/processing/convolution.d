a2ac8722f8b8e93535a69518c9b5c4d16fe2a2a2
VlasovRoman <vlasovroman.ru@yandex.ru>
2017-01-26 23:53:53 +0700

    elementaryConvolution now can use rgbManager

--- source/rip/processing/convolution.d
+++ source/rip/processing/convolution.d
@@ -12,11 +12,30 @@ private
 	import rip.processing.filters.linear;
 }
 
+version(RgbCachingOn) {
+	RGBColor result;
+
+	static this() {
+		result = new RGBColor(0, 0, 0);
+	}
+}
+
 // Точечная свертка
 auto elementaryConvolution(Signal, Filter)(Signal signal, Filter filter)
 {
 
-	RGBColor result = new RGBColor(0, 0, 0);
+
+
+	version(RgbCachingOn) {
+		result.setRed = 0;
+		result.setGreen = 0;	
+		result.setBlue = 0;
+	}
+	else {
+		RGBColor result = new RGBColor(0, 0, 0);
+	}
+
+
 
 	auto data = zip(signal, filter);
 	alias PairType = ElementType!(typeof(data));
@@ -35,7 +54,14 @@ auto elementaryConvolution(Signal, Filter)(Signal signal, Filter filter)
 
 	data.each!conv;
 
-	return result;
+
+	
+	version(RgbCachingOn) {
+		return RGBColor.getColor(result);
+	}
+	else {
+		return result;
+	}
 }
 
 // Свертка сигнала с линейным фильтром
@@ -60,11 +86,21 @@ auto convolve(Surface surface, LinearFilter filter)
 	return image;
 }
 
+auto convolutionNew(Signal)(Signal signal, LinearFilter filter)
+{
+	// Выполнение свертки на одном фрагменте диапазона окрестностей
+	auto performConvolution(Signal)(Signal signal, LinearFilter filter)
+	{
+		return filter.getOffset + filter.getDivider * elementaryConvolution(signal, filter.getKernel);
+	}
+	return signal.map!(a => performConvolution(a, filter));
+}
+
 auto convolveNew(Surface surface, LinearFilter filter)
 {
 	auto image = surface
 		.createFencesNew(filter.getWidth, filter.getHeight)
-			.convolution(filter)
+			.convolutionNew(filter)
 			.toSurface(surface.getWidth!int, surface.getHeight!int);
 	return image;
 }
